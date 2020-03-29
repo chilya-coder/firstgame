8f3adc177cb378140db26bd4706c9336e57636f6
Timur Dumanskiy <cap.tim.2001@gmail.com>
2020-03-29 22:35:28 +0300

    Enemies and hit were added

---
+++ Assets/Scripts/HeroController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rb;
+    Animator animator;
+    Vector2 startPosition;
+    const float MAX_SPEED = 10;
+    bool isGrounded;
+    float ySpeed;
+    float previosYSpeed;
+    public GameObject hit;
+    Vector2 hitPos;
+    float hitRate = 0.5f;
+    float canMakeNextHit = 0.5f;
+    bool isFacesRight { get; set; }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        isFacesRight = true;
+        previosYSpeed = 0;
+        startPosition = rb.position;
+    }
+    void Death()
+    {
+        rb.position = startPosition;
+        Vector3 localScale = transform.localScale;
+        localScale.x = -Mathf.Abs(localScale.x);
+        transform.localScale = localScale;
+        isFacesRight = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        Jump();
+        JumpAnimation();
+        Crawl();
+        ySpeed = (rb.position.y - previosYSpeed) * 100;
+        previosYSpeed = rb.position.y;
+        animator.SetFloat("ySpeed", ySpeed);
+        animator.SetBool("isCrawling", false);
+        if (Input.GetButtonDown("Fire1") && Time.time > canMakeNextHit)
+        {
+            canMakeNextHit = Time.time + hitRate;
+            Hit();
+        }
+        if (rb.position.y < -10)
+        {
+            Death();
+        }
+    }
+    private void FixedUpdate()
+    {
+    }
+    void Run()
+    {
+        animator.SetBool("isRunning", true);
+        float speed = Input.GetAxis("Horizontal");
+        if (speed != 0)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+        animator.SetFloat("speed", Mathf.Abs(speed));
+        rb.velocity = new Vector2(speed * MAX_SPEED, rb.velocity.y);
+        if (speed < 0 && isFacesRight || speed > 0 && !isFacesRight)
+        {
+            Flip();
+        }
+    }
+    void Flip()
+    {
+        isFacesRight = !isFacesRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x = -localScale.x;
+        transform.localScale = localScale;
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(transform.up * 12f, ForceMode2D.Impulse);
+        }
+
+    }
+
+    void JumpAnimation()
+    {
+        if (!isGrounded)
+        {
+            animator.SetBool("isJumping", true);
+        } else
+        {
+            animator.SetBool("isJumping", false);
+        }
+    }
+
+    void Crawl ()
+    {
+        if (Input.GetAxis("Vertical") < 0) {
+            animator.SetBool("isCrawling", true);
+        } else
+        {
+            animator.SetBool("isCrawling", false);
+        }
+    }
+    void Hit()
+    {
+        hitPos = transform.position;
+        if (isFacesRight)
+        {
+            hitPos.x += 1;
+            Instantiate(hit, hitPos, Quaternion.identity);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)//Передается когда входящий коллайдер контактирует с коллайдером данного объекта
+    {
+        if (collision.gameObject.tag == "Ground") // проверяем, стоит ли герой на объекте с тегом "Ground"
+        {
+            isGrounded = true; // если да - значит он приземлился
+        }
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Death();
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false; // нет - значит он еще в полете
+        }
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Death();
+        }
+    }
+}
---
+++ Assets/Scripts/HitScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rg;
+    void Start()
+    {
+        rg = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, 0.1f);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
