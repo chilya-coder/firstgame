21c96406c50ebd1462c8ac4f2b68be90d241cb81
chilya-coder <yuliya.chimyrys@gmail.com>
2020-03-30 20:41:42 +0300

    Camera script is added

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject hero;
+    // Start is called before the first frame update
+    void Update()
+    {
+        transform.position = new Vector3(hero.transform.position.x, hero.transform.position.y, -10f);
+    }
+
+}
